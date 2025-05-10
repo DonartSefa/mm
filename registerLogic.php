9e05a97465c8c4540dccd5cbb1f41a9841292e1b
DonartSefa <donart.sefa13@gmail.com>
2025-05-10 11:41:25 +0200

    1

--- registerLogic.php
+++ registerLogic.php
@@ -5,15 +5,27 @@ if(isset($_POST['submit'])){
     $emri = $_POST['emri'];
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $tempPass = $_POST['pasword'];
+    $tempPass = $_POST['password'];
     $password = password_hash($tempPass, PASSWORD_DEFAULT);
     $tempConfirm = $_POST['confirm_password'];
-    $confirm_password = password_hash($tempConfirm, PASSWORD_DEFAULT);
+    $confirm_password = password_hash($tempPass, PASSWORD_DEFAULT);
 
-    if(emty($emri ) || ($username ) || ($email ) || ($tempPassword ) || ($tempConfirm )){
+    if(empty($emri ) || ($username ) || ($email ) || ($tempPass ) || ($confirm_password )){
         echo "Please fill all the fields";
     }else{
-        $sql = "INSERT INTO users(emri, username, email, password, confirm_password) VALUES (:emri, :username, :email, :password, :confirm_password)";
+        $sql = "INSERT INTO users(emri, username, email, password, confirm_password, is_admin) VALUES (:emri, :username, :email, :password, :confirm_password, :is_admin)";
+        $insertSql = $conn->prepare($sql);
+
+        $insertSql->bindParam(":emri", $emri) ;
+        $insertSql->bindParam(":username", $username) ;
+        $insertSql->bindParam(":email", $email) ;
+        $insertSql->bindParam(":password", $password) ;
+        $insertSql->bindParam(":confirm_password", $confirm_password) ;
+        $insertSql->bindParam(":is_admin", 0) ;
+        
+        $insertSql->execute();
+
+        header("Location: login.php");
     }
 }
 
